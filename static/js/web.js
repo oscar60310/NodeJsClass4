be0ace8501fbb77e3b884b149ee57ba8552cfbfb
alice84329 <alice84329@gmail.com>
2017-04-14 18:58:29 +0800

    css

---
+++ static/js/web.js
@@ -0,0 +1,42 @@
+var wsManager;
+
+function WsManager() {
+    var self = this;
+    var protocol = (window.location.protocol == "https:") ? "wss://" : "ws://";
+    self.ws = new WebSocket(protocol + window.location.host + "/ws");
+    self.ws.onopen = function () {
+        // Web Socket is connected, send data using send()
+        console.log("[ws] connected.");
+        self.sendJson(self.ws, {
+            type: "LOGIN"
+        });
+    };
+    self.ws.onmessage = function (evt) {
+        var msg = JSON.parse(evt.data);
+        console.log(msg);
+        /*  */
+        switch (msg.type) {
+            case "LOGIN":
+                loginStatus(msg);
+                break;
+        }
+
+    };
+    self.ws.onclose = function () {
+        // websocket is closed.
+        console.log("ws closed.");
+    };
+    this.send = (data) => {
+        sendJson(self.ws, data);
+    }
+    this.sendJson = (ws, msg) => {
+        ws.send(JSON.stringify(msg));
+    }
+    loginStatus = (msg) => {
+        if (msg.status == "ok") {
+            setAlert(msg.name + "玩家，你好");
+        } else {
+            setAlert("<a href='" + msg.url + "' class=\"btn btn-primary btn-lg\" role=button>玩家 FB登入</a>");
+        }
+    }
+}
