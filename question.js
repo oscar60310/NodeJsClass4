ec4ee48d4072463b108c50406ad2aaf15f86f703
chenping.tsai <oscar60310@gmail.com>
2017-04-25 22:46:37 +0800

    getResult Function

--- question.js
+++ question.js
@@ -11,12 +11,22 @@ getQuestion = (token1, token2) => {
             currect: "B",
             time: 10,
             score: 10
+        }, {
+            id: 2,
+            description: {
+                text: '采楓是不是智障2?',
+                image: null
+            },
+            ans: { A: "不是", B: "是", C: "不是", D: "不是" },
+            currect: "B",
+            time: 10,
+            score: 10
         }];
         // +題目
-            axios.get('https://graph.facebook.com/v2.8/me/friends', { params: {access_token:token1} }).then((res) => {
-                console.log(res.data);
-                resolve(res.data.data.length > 0);
-            })
+        axios.get('https://graph.facebook.com/v2.8/me/friends', { params: { access_token: token1 } }).then((res) => {
+            console.log(res.data);
+            resolve(res.data.data.length > 0);
+        })
         resolve(questions);
     })
 }
--- wsConnect.js
+++ wsConnect.js
@@ -53,22 +53,41 @@ route = (ws, msg) => {
                 }
             });
             break;
-        case "ANSER":
-        
-        break;  
+        case "ANSWER":
+            gm.solveQuestion(ws, msg.choose).then((data) => {
+                if (data) {
+                    var g = gm.getGameByID(ws.gameid);
+                    sendResult(g, g.nowquestion);
+                }
+            }).catch((e) => { });
+            break;
+    }
+}
+sendResult = (game, nq) => {
+    console.log(game.nowquestion, nq);
+    if (game.nowquestion == nq) {
+        var re = { type: "RESULT", data: gm.getResult(game) };
+        sendJson(game.players[0].ws, re);
+        sendJson(game.players[1].ws, re);
+        setTimeout(sendQuestion,2000,game.id);
     }
 }
 sendQuestion = (gameid) => {
     gm.getQuestion(gameid).then((data) => {
-        var s = {
-            type: "QUESTION",
-            time: data.que.time,
-            que: data.que.description,
-            ans: data.que.ans,
-            id: data.que.id
-        };
-        sendJson(data.d[0], s);
-        sendJson(data.d[1], s);
+        if (data != null) {
+            var s = {
+                type: "QUESTION",
+                time: data.que.time,
+                que: data.que.description,
+                ans: data.que.ans,
+                id: data.que.id
+            };
+            sendJson(data.d[0], s);
+            sendJson(data.d[1], s);
+            var g = gm.getGameByID(gameid);
+            setTimeout(sendResult, s.time * 1000, g, g.nowquestion);
+        }
+
     });
 }
 notifyGetReady = (data) => {
